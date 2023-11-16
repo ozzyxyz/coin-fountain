b335158b6a3486ee43cc5a276cb3c155b7a75561
Berzan <berzan@tutanota.com>
2023-11-16 17:09:50 +0000

    add: content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import ButtonChain from "$lib/components/ButtonChain.svelte";
+    import ButtonWallet from "$lib/components/ButtonWallet.svelte";
+    import { injectedWallets } from "$lib/stores/injected-wallets";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        injectedWallets.exploreInjectedWallets();
+    });
+</script>
+
+<header class="sticky top-0 flex items-center justify-between h-16 px-4">
+    <a href="/" class="text-xl font-bold">Coin Fountain</a>
+
+    <div class="flex gap-4">
+        <ButtonWallet />
+        <ButtonChain />
+    </div>
+</header>
