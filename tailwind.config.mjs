dae2d6435afccef8ecabb9a49518b922fc610277
Berzan <berzan@tutanota.com>
2023-11-17 08:37:31 +0000

    add: Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,ts}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+}
